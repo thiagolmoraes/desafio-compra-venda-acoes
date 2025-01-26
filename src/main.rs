f1a74093ea79ceede0704601f728d1bf5cb676a8
Mangia <thiagolmoraes@gmail.com>
2025-01-26 11:14:05 -0300

    user flow - initial

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,35 @@
-use actix_web::{App, HttpServer};
+use actix_web::{App, HttpServer, middleware::Logger, web};
+use diesel::SqliteConnection;
+use diesel::r2d2::ConnectionManager;
 use anyhow::{Result, Context};
-use compra_venda_acoes::init_routes;
+use dotenvy::dotenv;
+use std::env;
+use env_logger::Env;
+use compra_venda_acoes::{init_routes, DbPool};
+
+
+pub fn establish_connection() -> DbPool {
+    dotenv().ok();
+    
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let manager = ConnectionManager::<SqliteConnection>::new(database_url);
+    
+    DbPool::builder()
+        .build(manager)
+        .expect("Pool created successfully")
+}
 
 #[actix_web::main]
 async fn main() -> Result<()> {
-    HttpServer::new(|| {
+
+    env_logger::init_from_env(Env::default().default_filter_or("debug"));
+
+    let pool = establish_connection();
+
+    HttpServer::new(move || {
         App::new()
+            .app_data(web::Data::new(pool.clone()))
+            .wrap(Logger::new("%a %r %s"))
             .configure(init_routes)
     })
     .bind("0.0.0.0:8080")
